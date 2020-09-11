e97dac0d4d1a8c00e6ef37159352d187ddd0f29f
Daniel.banks <regl5ld7sxcxx7popbsmxt2dtccph4jaxq5qnf7gbyyyrhk54dra>
2020-09-11 20:20:11 +0100

    Spring boot initializr

---
+++ src/test/java/com/github/danielmbanks/moviecatalog/MovieCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.danielmbanks.moviecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
