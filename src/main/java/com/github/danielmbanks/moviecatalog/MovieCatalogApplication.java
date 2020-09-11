e26fb82ed181f7ffa7b9b3a78580f2d167f81b3b
Daniel.banks <regl5ld7sxcxx7popbsmxt2dtccph4jaxq5qnf7gbyyyrhk54dra>
2020-09-11 20:33:41 +0100

    Cleaned up formatting and unnecessary maven properties

--- src/main/java/com/github/danielmbanks/moviecatalog/MovieCatalogApplication.java
+++ src/main/java/com/github/danielmbanks/moviecatalog/MovieCatalogApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieCatalogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieCatalogApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCatalogApplication.class, args);
+    }
 }
